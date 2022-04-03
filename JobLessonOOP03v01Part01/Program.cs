0abefa734f068691f33e1a2c965735bda5bc94c3
Safroniv <safroniv@mail.ru>
2022-04-04 00:13:43 +0300

    ООП Урок 3 Задание 1

--- JobLessonOOP03v01Part01/Program.cs
+++ JobLessonOOP03v01Part01/Program.cs
@@ -1,32 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace JobLessonOOP03v01Part01
 {
     class Program
     {
 
-            static void Main()
-            {
-                BankAccount mybankAccount0 = new BankAccount();
-                mybankAccount0.Print();
-                BankAccount mybankAccount1 = new BankAccount(50000.99m);
-                mybankAccount1.Print();
-                BankAccount mybankAccount2 = new BankAccount(TypeBankAccount.Debet);
-                mybankAccount2.Print();
-                BankAccount mybankAccount3 = new BankAccount(999_999.99m, TypeBankAccount.Credit);
-                mybankAccount3.Print();
+        static void Main()
+        {
+            BankAccount mybankAccount0 = new BankAccount();
+            mybankAccount0.Print();
+            BankAccount mybankAccount1 = new BankAccount(50000.99m);
+            mybankAccount1.Print();
+            BankAccount mybankAccount2 = new BankAccount(TypeBankAccount.Debet);
+            mybankAccount2.Print();
+            BankAccount mybankAccount3 = new BankAccount(999_999.99m, TypeBankAccount.Credit);
+            mybankAccount3.Print();
 
-                mybankAccount1.WithdrawFormBankAccount();
-                mybankAccount1.Print();
-                mybankAccount1.PutOnBankAccount();
-                mybankAccount1.Print();
+            mybankAccount1.WithdrawFormBankAccount();
+            mybankAccount1.Print();
+            mybankAccount1.PutOnBankAccount();
+            mybankAccount1.Print();
 
-                Console.ReadLine();
-            }
-        
+            Console.ReadLine();
+        }
+
     }
 }
