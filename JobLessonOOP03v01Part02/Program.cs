3180ed8f43c9598e8e212fd32ab25d833e97f7d9
Safroniv <safroniv@mail.ru>
2022-04-06 00:24:55 +0300

    ООП Урок 3 Задание 2 - готово.

--- JobLessonOOP03v01Part02/Program.cs
+++ JobLessonOOP03v01Part02/Program.cs
@@ -10,6 +10,19 @@ namespace JobLessonOOP03v01Part02
     {
         static void Main(string[] args)
         {
+            string string001 = "АбВгДеЖз";
+            Console.WriteLine(Reverse(string001));
+            string string002 = "String To Reverse TEST 002";
+            Console.WriteLine(Reverse(string002));
+            string string003 = Convert.ToString(Console.ReadLine());
+            Console.WriteLine(Reverse(string003));
+            Console.ReadLine();
+        }
+        static string Reverse (string string1)
+        {
+            char[] arrayChars = string1.ToCharArray();
+            Array.Reverse(arrayChars);
+            return new string(arrayChars);
         }
     }
 }
