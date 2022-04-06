6711d9f10dc649695be8a4baa5bc5c5c3ce91ad3
Safroniv <safroniv@mail.ru>
2022-04-06 20:58:49 +0300

    ООП Урок 3 Задание 3.

---
+++ JobLessonOOP03v01Part03/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobLessonOOP03v01Part03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
